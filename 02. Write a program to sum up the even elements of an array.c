712677a11c8b55857c8674850a57965fe93f8c68
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-29 23:52:42 +0600

    Update 02. Write a program to sum up the even elements of an array.c

--- 02. Write a program to sum up the even elements of an array.c
+++ 02. Write a program to sum up the even elements of an array.c
@@ -1,19 +1,19 @@
 #include <stdio.h> 
-int main()
-{ 
+int main(){ 
 int i,n,sum=0; 
-printf("Enter any array size: "); 
-scanf("%d",&n); 
+  printf("Enter any array size: "); 
+  scanf("%d",&n); 
 int arr[n]; 
-printf("Enter %d Numbers: ", n); 
+  printf("Enter %d Numbers: ", n); 
 for(i=0; i<n; i++)
 { 
-scanf("%d", &arr[i]);
+  scanf("%d", &arr[i]);
 }
 for(i=0; i<n; i++)
 { 
 if(arr[i]%2==0) 
-sum=sum+arr[i];
+  sum=sum+arr[i];
 } 
-printf("Total Sum of Even values is: %d ",sum); 
-return 0; } 
+  printf("Total Sum of Even values is: %d ",sum); 
+return 0;
+} 
